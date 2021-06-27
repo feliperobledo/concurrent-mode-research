fc9009f29381f79a07c396d8cc8c7ddefe84c9b9
Felipe Robledo <felipe.robledo@prezi.com>
2021-06-27 14:05:26 -0400

    chore: move list example to own module

---
+++ src/SuspenseListExample.tsx
@@ -0,0 +1,43 @@
+import React, { Suspense } from "react";
+
+import { Img, resource } from "react-suspense-img";
+
+const FALLBACK = (
+  <div
+    style={{
+      display: "inline-block",
+      width: 64,
+      height: 64,
+      backgroundColor: "#ccc",
+    }}
+  />
+);
+
+const IMAGES = [
+  "https://placekitten.com/400/400",
+  "https://placekitten.com/401/401",
+  "https://placekitten.com/402/402",
+  "https://placekitten.com/403/403",
+  "https://placekitten.com/404/404",
+  "https://placekitten.com/405/405",
+  "https://placekitten.com/406/406",
+  "https://placekitten.com/407/407",
+  "https://placekitten.com/408/408",
+  "https://placekitten.com/409/409",
+];
+
+export const SuspenseListExample = () => {
+  return (
+    <React.SuspenseList revealOrder="forwards" tail="collapsed">
+      {IMAGES.map((src) => {
+        resource.preloadImage(src);
+
+        return (
+          <React.Suspense fallback={FALLBACK} key={src}>
+            <Img src={src} width={64} alt="Kitten" />
+          </React.Suspense>
+        );
+      })}
+    </React.SuspenseList>
+  );
+};
