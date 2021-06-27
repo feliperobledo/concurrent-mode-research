8c4803021eb30ccfe2b96e716672f82b87a2bca4
Felipe Robledo <felipe.robledo@prezi.com>
2021-06-27 14:20:18 -0400

    feat: integrate a tab area

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,38 @@ import React, { Suspense, lazy } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import {
+  Provider,
+  defaultTheme,
+  Button,
+  Item,
+  TabList,
+  TabPanels,
+  Tabs,
+} from "@adobe/react-spectrum";
+
 import { SuspenseListExample } from "./SuspenseListExample";
 
 //const GameProfile = lazy(() => import("./GameProfile"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <SuspenseListExample />
-      </header>
-    </div>
+    <Provider theme={defaultTheme}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Tabs aria-label="History of Ancient Rome">
+          <TabList>
+            <Item key="FoR">Founding of Rome</Item>
+            <Item key="MaR">Monarchy and Republic</Item>
+            <Item key="Emp">Empire</Item>
+          </TabList>
+          <TabPanels>
+            <Item key="Emp">
+              <SuspenseListExample />
+            </Item>
+          </TabPanels>
+        </Tabs>
+      </Suspense>
+    </Provider>
   );
 }
 
